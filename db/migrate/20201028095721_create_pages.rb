0bc9eb3e0347a057e19c5e6d3b7d0972b3ccbc13
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-10-28 11:03:19 +0100

    Add pages scaffold

---
+++ db/migrate/20201028095721_create_pages.rb
@@ -0,0 +1,10 @@
+class CreatePages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pages do |t|
+      t.string :email
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
