86af40da9e64464fe6df7f472191a9bdd8d39955
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-11-04 08:20:59 +0100

    Add header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,7 @@ class PagesController < ApplicationController
 
   # GET /pages/new
   def new
-    @page = Page.new
+    # @page = Page.new
   end
 
   # GET /pages/1/edit
@@ -23,42 +23,42 @@ class PagesController < ApplicationController
 
   # POST /pages
   # POST /pages.json
-  def create
-    @page = Page.new(page_params)
+  # def create
+  #   @page = Page.new(page_params)
 
-    respond_to do |format|
-      if @page.save
-        format.html { redirect_to @page, notice: 'Page was successfully created.' }
-        format.json { render :show, status: :created, location: @page }
-      else
-        format.html { render :new }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @page.save
+  #       format.html { redirect_to @page, notice: 'Page was successfully created.' }
+  #       format.json { render :show, status: :created, location: @page }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @page.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /pages/1
   # PATCH/PUT /pages/1.json
   def update
-    respond_to do |format|
-      if @page.update(page_params)
-        format.html { redirect_to @page, notice: 'Page was successfully updated.' }
-        format.json { render :show, status: :ok, location: @page }
-      else
-        format.html { render :edit }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+    #   if @page.update(page_params)
+    #     format.html { redirect_to @page, notice: 'Page was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @page }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @page.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /pages/1
   # DELETE /pages/1.json
   def destroy
-    @page.destroy
-    respond_to do |format|
-      format.html { redirect_to pages_url, notice: 'Page was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    # @page.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to pages_url, notice: 'Page was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
